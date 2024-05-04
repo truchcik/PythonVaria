bd5187604e33c707d144a0dbd966fb0a6ab459b2
truchcik <85064419+truchcik@users.noreply.github.com>
2024-05-04 16:11:22 +0200

    Update P4_Check_Todays_Submits.py changed how you define start date
    You set how many days we go back and hour from which it should start

--- P4_Check_Todays_Submits.py
+++ P4_Check_Todays_Submits.py
@@ -1,45 +1,38 @@
 import time, datetime
 from P4 import P4
 
-def getTodayTime(startHour):
-    #today = datetime.date.today()
-    today = datetime.datetime.strptime('26 Sep 2012', '%d %b %Y')
-    newdatetime = today.replace(hour=11, minute=59)
-
-
-def getCheckFrom(checkNDays):
-    tNow = (time.time())
-    tCheckFrom = tNow - (24 * 60 * 60 * checkNDays)
-    print ('Checking sumbits between {} and {}'.format(datetime.datetime.fromtimestamp(tNow),
-                                                       datetime.datetime.fromtimestamp(tCheckFrom)))
+def getCheckFromDays(hr, min=0, daysBack =0, ):
+    # today = datetime.datetime.strptime('26 Sep 2012', '%d %b %Y')
+    today = datetime.datetime.today().replace(hour=hr, minute=min, second=0, microsecond=0) - datetime.timedelta(days = daysBack)
+    tCheckFrom = datetime.datetime.timestamp(today)
     return tCheckFrom
 
-checkUsrs = ['DoMil']   #users to check for submits
-checkNDays = .06       #how many days to check
-validDesc = 'Validated on buildmachine' #properly validated CL should have this in description
 
-tCheckFrom = getCheckFrom(checkNDays)
-getTodayTime('10:30')
+checkUsrs = ['DoMil', 'Kokosz']   #users to check for submits
+validDesc = 'Validated on buildmachine' #properly validated CL should have this in description
+checkPath = '//streamsDomi/mainlineDomi/01_learn/...'
+
+tCheckFrom = getCheckFromDays(hr=13, min=5, daysBack = 0)
 
 p4 = P4()
-p4.port = "1666"
+p4.port = '1666'
 p4.connect()
 
-#with time restriction, to search bigger databases
-#info = p4.run("changes", "-s", "submitted", "//streamsDomi/mainlineDomi/01_learn/...@2020/01/01,@2024/05/05")
-info = p4.run("changes",  "-s", "submitted")
+#info = p4.run('changes', '-s', 'submitted', '{}@2020/01/01,@2024/05/05'.format(checkPath))  #with time restriction, to search bigger databases
+info = p4.run('changes',  '-s', 'submitted', checkPath)
 
-print ('\nCL submitted within {} days from now, not having "{}" in desc:\n'.format(checkNDays,validDesc))
-for i in info:
-    for usr in checkUsrs: #checking users from list
+print ('\nCL submitted after {}, not having "{}" in desc:'.format(datetime.datetime.fromtimestamp(tCheckFrom),validDesc))
+for usr in checkUsrs:  # checking users from list
+    print('\nUnvalidated CLs from user "{}":'.format(usr))
+    for i in info:
         if i['user']==usr:
             tSubmitted = int(i['time'])
             if tSubmitted > tCheckFrom:
-                print ('\tUser name:', i['user'], end='')
+                #print ('\tUser name:', i['user'], end='')
                 #print('Submit time:', datetime.datetime.fromtimestamp(tSubmitted))
                 #print ('Path:', i['path'])
                 #print('Workspace:', i['client'])
-                print('\tDesc: "{}"'.format(i['desc'].strip()))
+                print('\tDesc: "{}". \tSubmitted: {}'.format( i['desc'].strip(), datetime.datetime.fromtimestamp(tSubmitted) ) )
 
 p4.disconnect()
 
